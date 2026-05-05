d9b513b522b2266ec0cac7d4aed2b13a762aba92
hahnguil <guilherme.f.h@hotmail.com>
2026-05-04 23:18:18 -0300

    build: removendo condicional que quebrada o proxy

--- src/server.ts
+++ src/server.ts
@@ -84,15 +84,14 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point, or it is ran via PM2.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = Number(process.env['PORT']) || 4000;
-  app.listen(port, '0.0.0.0', (error?: Error) => {
+const port = Number(process.env['PORT']) || 4000;
+
+app.listen(port, '0.0.0.0', (error?: Error) => {
   if (error) {
     throw error;
   }
   console.log(`Node Express server listening on port ${port}`);
 });
-}
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
